74d7187c033d99cc6fab71a04af74ac07bc84b12
Hunnain Pasha <hunnainpashahgchgc@gmail.com>
2019-01-10 03:43:01 +0500

    App

---
+++ App.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+import ScreenRouter from './Routers/routers';
+
+export default class App extends Component {
+  render() {
+    return (
+       <ScreenRouter />
+    );
+  }
+}
